00f0754f02ca30c36fd4825b2a18aa3c5e69157b
rodesdkeu <rodesdkeu@gmail.com>
2015-12-31 15:28:14 +0200

    Simple Drag & Drop sample

---
+++ Drag_and_Drop/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drag_and_Drop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            //pictureBox2.
+        }
+
+    
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox1.DoDragDrop(pictureBox1.Image, DragDropEffects.Copy);
+
+        }
+
+        private void pictureBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void pictureBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            pictureBox2.Image = (Image)e.Data.GetData(DataFormats.Bitmap);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox2.AllowDrop = true;
+        }
+    }
+}
